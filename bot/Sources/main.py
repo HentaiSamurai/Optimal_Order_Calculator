185718b5c0f09fd94383d09e4f6a4b987f550aed
33kingusp <33kingusp@gmail.com>
2021-02-28 22:11:11 +0900

    「もゆさまおるかー」の機能を変更

--- bot/Sources/main.py
+++ bot/Sources/main.py
@@ -4,6 +4,9 @@ import analysis
 
 client = discord.Client()
 
+# botが稼働しているチャンネル
+current_channel = None
+
 # ログインしたとき
 @client.event
 async def on_ready():
@@ -12,21 +15,31 @@ async def on_ready():
 # だれかが発言したとき
 @client.event
 async def on_message(message):
+	global current_channel
+
 	if message.author.bot:
 		return
 	print(message.author.id, message.content, message.attachments)
 
-	if message.content == '.picture': # 画像受け取りのテスト
+	if message.content == '.もゆさまおるかー':
+		current_channel = message.channel
+		print(current_channel.name, 'にいるわ')
+		await current_channel.send('いるわよ')
+
+	# もゆさまは呼ばないと出てきてくれない
+	# 別のチャンネルでは反応しない
+	if current_channel is None or current_channel != message.channel:
+		return
+	
+	elif message.content == '.picture': # 画像受け取りのテスト
 		if len(message.attachments) == 0:
-			await message.channel.send('画像がないわね...')
+			await current_channel.send('画像がないわね...')
 		else:
 			for attachment in message.attachments:
 				img = analysis.imread_web(attachment.url)
-			await message.channel.send('受け取ったわよ')
-	elif message.content == '.もゆさまおるかー':
-		await message.channel.send('いるわよ')
+			await current_channel.send('受け取ったわよ')
 	elif message.content == '.じゃあの':
-		await message.channel.send('はいはーい')
+		await current_channel.send('はいはーい')
 		await client.logout()
 		await sys.exit()
 
