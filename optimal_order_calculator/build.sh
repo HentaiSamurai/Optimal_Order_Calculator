0bb2083d2ca2c07d431b954675054e431d48b8b0
HentaiSamurai <saionzi0001@gmail.com>
2021-03-01 11:01:39 +0900

    hello world for build test

---
+++ optimal_order_calculator/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#ビルドして帰ってくる
+
+mkdir build
+cd build
+cmake ../
+make
+cd ../
---
+++ optimal_order_calculator/main.cpp
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <stdio.h>
+#include <iomanip>
+
+#include <vector>
+
+
+
+int main(int argc, char* argv[]){
+
+	std::cout<<"hello"<<std::endl;
+
+	return 0;
+}
+
